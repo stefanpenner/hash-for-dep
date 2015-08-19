fdad06bcbbe995e2a6f35726f2b80a0529b87378
Chad Hietala <chadhietala@gmail.com>
2015-08-19 13:22:24 -0700

    Add logging around recursive discovery and hashing

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 var helpers = require('broccoli-kitchen-sink-helpers');
 var crypto = require('crypto');
 var statPathsFor = require('./lib/stat-paths-for');
+var timing = require('./lib/timing');
+var debug = require('debug')('hash-for-dep:index');
 
 /* @public
  *
@@ -12,8 +14,18 @@ var statPathsFor = require('./lib/stat-paths-for');
  * @return {String} a hash representing the stats of this module and all its descendents
  */
 module.exports = function hashForDep(name, dir, _hashTreeOverride) {
+  var start = timing.start();
   var inputHashes = statPathsFor(name, dir).map(_hashTreeOverride || helpers.hashTree).join(0x00);
+  debug('inputHashes stats: %o', {
+    hashes: inputHashes.length,
+    elapsedTime: timing.end(start)
+  });
 
-  return crypto.createHash('md5').
-    update(inputHashes).digest('hex');
+  start = timing.start();
+  var hash = crypto.createHash('md5').update(inputHashes).digest('hex');
+  debug('hash stats: %o', {
+    elapsedTime: timing.end(start)
+  });
+
+  return hash;
 };
