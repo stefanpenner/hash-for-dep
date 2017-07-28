b9ae3c398d9b5a74940569debdcfe2750af8b7ff
zkochan <zoltan.kochan@gmail.com>
2017-07-29 02:12:24 +0300

    test: symlinks are not preserved during resolution

--- tests/resolve-pkg-test.js
+++ tests/resolve-pkg-test.js
@@ -1,11 +1,35 @@
 'use strict';
+var fs = require('fs');
 var path = require('path');
 var assert = require('assert');
 var resolvePkg = require('../lib/resolve-pkg');
 
 var fixturesPath = path.join(__dirname, 'fixtures');
 
+var symlinkDir = path.join(fixturesPath, '..', 'symlink');
+
 describe('resolvePkg', function() {
+  before(function () {
+    try {
+      fs.unlinkSync(symlinkDir);
+    } catch (err) {
+      if (err.code !== 'ENOENT') {
+        throw err;
+      }
+    }
+
+    try {
+        fs.symlinkSync('./fixtures/node_modules/dedupped', symlinkDir, 'dir');
+    } catch (err) {
+        // if fails then it is probably on Windows and lets try to create a junction
+        fs.symlinkSync(path.join(fixturesPath, 'node_modules', 'dedupped') + '\\', symlinkDir, 'junction');
+    }
+  });
+
+  after(function () {
+    fs.unlinkSync(symlinkDir);
+  });
+
   it('Resolves to the absolute main entry point path of the provided package', function() {
     assert.equal(resolvePkg('./'), path.resolve('./'));
     assert.equal(resolvePkg('foo', fixturesPath), path.join(fixturesPath, 'node_modules/foo'));
@@ -14,5 +38,9 @@ describe('resolvePkg', function() {
 
   it('Resolves to the module directory if no main entry is defined', function() {
     assert.equal(resolvePkg('no-main/', fixturesPath), path.join(fixturesPath, 'node_modules/no-main/'));
+  });
+
+  it('Don\'t preserve symlinks when resolving package', function() {
+    assert.equal(resolvePkg('foo', symlinkDir), path.join(fixturesPath, 'node_modules/foo'));
   });
 });
