b2838a1f579df8a0b97e70e4cdf875ac71bd9e72
Travis Hoover <thoov7@gmail.com>
2019-02-06 14:51:38 -0800

    Allow optionalDependencies to be included via options flag

--- tests/deps-for-test.js
+++ tests/deps-for-test.js
@@ -39,4 +39,52 @@ describe('depsFor', function() {
 
     assert.deepEqual(depsFor('foo', fixturesPath), expectedDeps);
   });
+
+  it('Allows optionalDependencies to be included via an option', function() {
+    var expectedDeps = [{
+      name: 'optional',
+      version: '1.0.0',
+      main: 'index.js',
+      dependencies: {
+        bar: '1.0.0',
+        dedupped: '1.0.0',
+        missing: '1.0.0'
+      },
+      optionalDependencies: {
+        'optional-foo': '1.0.0'
+      },
+      baseDir: path.join(fixturesPath, '/node_modules/optional/')
+    }, {
+      name: 'optional-foo',
+      version: '1.0.0',
+      main: 'index.js',
+      baseDir: path.join(fixturesPath, '/node_modules/optional/node_modules/optional-foo/')
+    }, {
+      name: 'bar',
+      version: '1.0.0',
+      main: 'index.js',
+      baseDir: path.join(fixturesPath, '/node_modules/optional/node_modules/bar/')
+    }, {
+      name: 'dedupped',
+      version: '1.0.0',
+      main: 'index.js',
+      dependencies: {
+        'dedupped-child': '1.0.0'
+      },
+      baseDir: path.join(fixturesPath, '/node_modules/dedupped/')
+    }, {
+      name: 'dedupped-child',
+      version: '1.0.0',
+      main: 'index.js',
+      baseDir: path.join(fixturesPath, '/node_modules/dedupped/node_modules/dedupped-child/')
+    }];
+
+    assert.deepEqual(depsFor('optional', fixturesPath, { includeOptionalDeps: true }), expectedDeps);
+
+    // remove "optional-foo" from the expectedDeps array
+    expectedDeps.splice(expectedDeps.findIndex(dep => dep.name === 'optional-foo'), 1);
+
+    assert.deepEqual(depsFor('optional', fixturesPath, { includeOptionalDeps: false }), expectedDeps);
+  });
+
 });
